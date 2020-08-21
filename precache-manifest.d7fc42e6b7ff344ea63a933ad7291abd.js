72837b9c6580283eef3b9994b9030838dd265b9e
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-21 16:32:14 -0700

    Updates

---
+++ precache-manifest.d7fc42e6b7ff344ea63a933ad7291abd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5d63586a59fe05276482a287496e10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e483d2a423c554d13ac8",
+    "url": "/static/css/main.5f24e4dd.chunk.css"
+  },
+  {
+    "revision": "f374a39ae5a5f080a0e3",
+    "url": "/static/js/2.364953c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.364953c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e483d2a423c554d13ac8",
+    "url": "/static/js/main.0f6b50b7.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e43bbf650398a26aa728ebd765805f67",
+    "url": "/static/media/skies-ani.e43bbf65.gif"
+  }
+]);
