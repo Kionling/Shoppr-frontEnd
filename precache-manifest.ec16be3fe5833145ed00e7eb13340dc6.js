5efc99aa5b4ecae54029354344e8cd88e73819dc
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-21 16:40:22 -0700

    Updates

---
+++ precache-manifest.ec16be3fe5833145ed00e7eb13340dc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a05b1c49113f36d5529cb636954735",
+    "url": "/Shoppr-frontEnd/index.html"
+  },
+  {
+    "revision": "e1ec9d7b54da2ee7dd7c",
+    "url": "/Shoppr-frontEnd/static/css/main.5f24e4dd.chunk.css"
+  },
+  {
+    "revision": "f374a39ae5a5f080a0e3",
+    "url": "/Shoppr-frontEnd/static/js/2.364953c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Shoppr-frontEnd/static/js/2.364953c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ec9d7b54da2ee7dd7c",
+    "url": "/Shoppr-frontEnd/static/js/main.ac099b21.chunk.js"
+  },
+  {
+    "revision": "6706f52fcfd2523742b1",
+    "url": "/Shoppr-frontEnd/static/js/runtime-main.87c110e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Shoppr-frontEnd/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e43bbf650398a26aa728ebd765805f67",
+    "url": "/Shoppr-frontEnd/static/media/skies-ani.e43bbf65.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7fc42e6b7ff344ea63a933ad7291abd.js"
+  "/Shoppr-frontEnd/precache-manifest.ec16be3fe5833145ed00e7eb13340dc6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Shoppr-frontEnd/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
